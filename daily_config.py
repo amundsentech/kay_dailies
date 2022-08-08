3581e52a87a0bd40a5f03b3fd5a50e4e661b3fee
sebastian coombs <63413957+sebastiancoombs@users.noreply.github.com>
2022-08-08 16:56:55 -0600

    Update daily_config.py

--- daily_config.py
+++ daily_config.py
@@ -1,12 +1,29 @@
+import importlib
+import subprocess
 
-import pandas as pd
-import numpy as np
-from tqdm.notebook import tqdm_notebook as tqdm
-tqdm.pandas()
-from tqdm import tqdm
-import os
-import getopt,sys
+import sys
 
+pkgs = {'pandas': 'pd', 'tqdm': 'tqdm','numpy':'np','openpyxl':'openpyxl','openpyxl':'openpyxl'}
+def check_packages():
+    for p in pkgs:
+        s = pkgs[p]
+        try:
+            print(f'check for {p}')
+            s = importlib.import_module(p)
+            
+        except ImportError:
+            print(f'{p} is not installed and has to be installed')
+            subprocess.call([sys.executable, '-m', 'pip', 'install', p])
+        finally:
+            s = importlib.import_module(p)
+            print(f'{p} is properly installed')
+    return
+try:
+    check_packages()
+except:
+    print('upgrade pip then try again')
+    subprocess.check_call([sys.executable,'-m','pip','install','--upgrade','pip'])
+    check_packages()
 
 path='/Volumes/GoogleDrive/Shared drives/AMC Projects/_AZ_Kay/_Drilling/_Shift Reports/'
 
